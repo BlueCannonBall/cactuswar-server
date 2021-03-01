af5d76d68bb1f925f898bfc5539310261e03d252
BlueCannonBall <35404206+BlueCannonBall@users.noreply.github.com>
2021-03-01 13:54:07 -0500

    formatting, stopping micro commits

--- core.hpp
+++ core.hpp
@@ -29,10 +29,12 @@ unsigned int get_uuid() {
 }
 
 inline bool aabb(qt::Rect rect1, qt::Rect rect2) {
-    return (rect1.x < rect2.x + rect2.width &&
-    rect1.x + rect1.width > rect2.x &&
-    rect1.y < rect2.y + rect2.height &&
-    rect1.y + rect1.height > rect2.y);
+    return (
+        rect1.x < rect2.x + rect2.width &&
+        rect1.x + rect1.width > rect2.x &&
+        rect1.y < rect2.y + rect2.height &&
+        rect1.y + rect1.height > rect2.y
+    );
 }
 
 class Vector2 {
