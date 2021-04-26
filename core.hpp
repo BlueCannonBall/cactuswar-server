f020acfdf157ad09ebe5bf39c7ef157d82b17b28
BlueCannonBall <35404206+BlueCannonBall@users.noreply.github.com>
2021-04-26 15:15:12 -0400

    gcc winfan magik

--- core.hpp
+++ core.hpp
@@ -341,7 +341,7 @@ class Arena {
 
         Entities entities;
         qt::Quadtree tree = qt::Quadtree(qt::Rect {.x = 0, .y = 0, .width = ARENA_SIZE, .height = ARENA_SIZE}, 10, 4);
-        unsigned int target_shape_count = 125;
+        unsigned int target_shape_count = 2000;
 #ifdef THREADING
         mutex qtmtx;
 #endif
@@ -424,7 +424,7 @@ class Arena {
             // INFO("Mouse position: " << entities.players[player_id]->input.mousepos);
         }
         
-        void update() {
+        void update() __attribute__((hot)) {
             if (entities.players.size() == 0) {
                 return;
             }
