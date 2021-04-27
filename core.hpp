be449218d72b4178253ac137d935f92f1711497c
BlueCannonBall <35404206+BlueCannonBall@users.noreply.github.com>
2021-04-27 10:14:32 -0400

    made run cold

--- core.hpp
+++ core.hpp
@@ -612,7 +612,7 @@ class Arena {
 #endif
         }
 
-        void run(ws28::Server& server, unsigned short port) {
+        void run(ws28::Server& server, unsigned short port) __attribute__((cold)) {
             for (unsigned int i = 0; i<target_shape_count; i++) {
                 Shape *new_shape = new Shape;
                 new_shape->id = get_uid();
