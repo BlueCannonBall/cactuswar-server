077d6203a3ed1832f46f91fddb1bcbcc6184d982
BlueCannonBall <35404206+BlueCannonBall@users.noreply.github.com>
2021-07-09 14:48:14 -0400

    formatting

--- core.hpp
+++ core.hpp
@@ -57,15 +57,15 @@ inline bool file_exists(const std::string& name) {
 }
 
 template<typename A, typename B>
-std::pair<B,A> flip_pair(const std::pair<A,B>& p) {
-    return std::pair<B,A>(p.second, p.first);
+std::pair<B, A> flip_pair(const std::pair<A,B>& p) {
+    return std::pair<B, A>(p.second, p.first);
 }
 
 template<typename A, typename B>
-std::multimap<B,A> flip_map(const std::map<A,B>& src) {
-    std::multimap<B,A> dst;
+std::multimap<B, A> flip_map(const std::map<A,B>& src) {
+    std::multimap<B, A> dst;
     std::transform(src.begin(), src.end(), std::inserter(dst, dst.begin()),
-        flip_pair<A,B>);
+        flip_pair<A, B>);
     return dst;
 }
 
