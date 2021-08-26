dc037101ad68979c9502444b221deff7bf1da70b
BlueCannonBall <mmttiizz100@gmail.com>
2021-08-26 14:36:23 -0400

    ???

--- main.cpp
+++ main.cpp
@@ -171,8 +171,8 @@ int main(int argc, char** argv) {
             ip = client->GetIP();
         }
         db->Get(leveldb::ReadOptions(), ip, &value);
-        if (s.ok() && value.size() != 0) {
-            if (value != "0") {
+        if (s.ok()) {
+            if (value == "1") {
                 BRUH("BANNED PLAYER TRIED TO CONNECT, REJECTING CONNECTION");
                 return false;
             }
